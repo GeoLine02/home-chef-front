5ca45e3e1044d758c9cac4176bbcfca57afb0f50
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-09-06 21:47:39 +0400

    style: product details, auth, cart modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+// import AuthModal from "@/components/auth/AuthModal";
+// import CartList from "@/components/cart/CartList";
+
+import ProductDetailsModal from "@/components/product/ProductDetailsModal";
+
 export default function Home() {
   return (
     <div className="">
       <h1>HOME PAGE</h1>
+      {/* <CartList isCartOpen={true} layout="side" /> */}
+      {/* <AuthModal /> */}
+      <ProductDetailsModal />
     </div>
   );
 }
